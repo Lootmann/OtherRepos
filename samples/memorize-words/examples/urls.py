9371fd0d0a1fd1d4af71f22bf2ab4562a015c9c2
Lootmann <lyotsu.programming.or.some@gmail.com>
2023-07-09 00:34:59 +0900

    examples: create examples

--- samples/memorize-words/examples/urls.py
+++ samples/memorize-words/examples/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from examples.views import ExampleIndexView
+from examples.views import ExampleIndexView, ExampleListView
 
 app_name = "examples"
 
 urlpatterns = [
     path("", ExampleIndexView.as_view(), name="index"),
+    path("list/", ExampleListView.as_view(), name="list"),
 ]
